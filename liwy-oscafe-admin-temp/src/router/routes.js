aeaa786b5821e32488f6c17a2f08b4b6c8ce800d
wenyao02.li <wenyao02.li@vipshop.com>
2018-11-12 17:02:07 +0800

    修改路由配置增加图标信息

--- liwy-oscafe-admin-temp/src/router/routes.js
+++ liwy-oscafe-admin-temp/src/router/routes.js
@@ -3,19 +3,23 @@ import Example from '@/components/Example';
 export const routes = [{
     path: '/temp',
     name: '示例页面',
+    icon: 'el-icon-setting',
     component: portalSDK.AppMain,
     children: [{
         path: '/example',
         name: '示例1',
+        icon: 'el-icon-document',
         component: Example,
         leaf: true
     }, {
         path: '/example2',
         name: '示例2',
+        icon: 'el-icon-message',
         component: portalSDK.AppMainChildren,
         children: [{
             path: '/example2_1',
             name: '示例2-1',
+            icon: 'el-icon-caret-right',
             component: portalSDK.AppMainChildren,
             leaf: true,
             redirect: '/example2_1/list',
